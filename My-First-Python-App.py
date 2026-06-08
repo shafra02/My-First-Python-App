11a62322b1f0c211ef523f13b4d4c85000296d06
shafra02 <mobarmegel@gmail.com>
2026-06-09 00:37:27 +0300

    Create My-First-Python-App.py

---
+++ My-First-Python-App.py
@@ -0,0 +1,18 @@
+#name=input("enter your name :" )
+#print("your name is "+name)
+
+#age=input("enter your age :")
+#print("your age is :" + age)
+
+
+print("| welcome to calculator app | ")
+num1= float(input("first number : "))
+num2= float(input("second number : "))
+print("-----------------------------------")
+
+print("adding + :" + str(num1+num2))
+print("subtraction - :"+ str(num1-num2))
+print("multiply * :"+ str(num1*num2))
+print("divide / :"+ str(num1/num2))
+print("modulus % :"+ str(num1%num2))
+
